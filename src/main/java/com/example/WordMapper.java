a1d2e0c7dfd94e600bc30e35f2fd15b176bf1231
S S Nikil Teja <85442051+nikilteja15@users.noreply.github.com>
2025-02-11 17:52:09 +0000

    Successfully Finished

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,20 @@
 package com.example;
 
-public class WordMapper{
-    
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase()); // Remove punctuation, lowercase
+            context.write(word, one);
+        }
+    }
 }
